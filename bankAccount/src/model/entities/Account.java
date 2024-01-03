122357d95a1c87fa07f85d18289862983124c102
Matheus Farias <matheusaf.contato@gmail.com>
2024-01-03 16:37:10 -0300

    bank account exception handling

--- bankAccount/src/model/entities/Account.java
+++ bankAccount/src/model/entities/Account.java
@@ -1,5 +1,55 @@
 package model.entities;
 
+import model.exception.DomainException;
+
 public class Account {
+
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withDrawLimit;
+
+	public Account() {
+
+	}
+
+	public Account(Integer number, String holder, Double balance, Double withDrawLimit) {
+
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withDrawLimit = withDrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public Double getWithDrawLimit() {
+		return withDrawLimit;
+	}
+
+	public void deposit(Double amount) {
+		balance += amount;
+	}
+
+	public void withDraw(Double amount) {
+
+		if (amount > balance) {
+			throw new DomainException("Not enough balance");
+		} else if (amount > withDrawLimit) {
+			throw new DomainException("The amount exceeds withdraw limit");
+		}
+
+		balance -= amount;
+	}
 
 }
